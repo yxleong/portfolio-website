91fcbee8a7360b67335d7a1e11562afc2d4676cb
yxleong <yx0829leong@gmail.com>
2024-08-01 08:54:20 +0800

    profile pic uploaded

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "dl.dropboxusercontent.com",
+        hostname: "raw.githubusercontent.com",
       },
     ],
   },
